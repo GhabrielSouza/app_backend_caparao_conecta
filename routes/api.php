801ec7ecebdf0707f03329a0c9435871fcf45882
ghabriel <souzaghabriel195@gmail.com>
2025-03-27 19:44:44 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/status', function () {
-    return 'Status OK';
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
