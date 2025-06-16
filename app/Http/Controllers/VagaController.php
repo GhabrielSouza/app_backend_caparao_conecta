b87a79c65894dfbf9d361c2dd8fdca60fe7d654b
LabMaker Ifes Alegre <labmaker@MacBook-Air-de-LabMaker.local>
2025-06-16 17:20:35 -0300

    alterando os gets de vagas

--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -28,7 +28,7 @@ class VagaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    
+
     public function store(Request $request)
     {
         // 1. ATUALIZAR REGRAS DE VALIDAÇÃO PARA IDs
@@ -39,18 +39,18 @@ class VagaController extends Controller
             'data_fechamento' => 'required|date',
             'qtd_vaga' => 'required|integer',
             'modalidade_da_vaga' => 'required|string|max:255',
-            'id_empresas' => 'required|integer|exists:empresas,id_pessoas', 
-           
+            'id_empresas' => 'required|integer|exists:empresas,id_pessoas',
+
             'habilidades' => 'nullable|array',
             'cursos' => 'nullable|array',
         ];
-    
+
         $validator = Validator::make($request->all(), $rules);
-    
+
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 422);
         }
-    
+
         // A lógica de criação da vaga permanece a mesma
         $vaga = new Vaga;
         $vaga->titulo_vaga = $request->titulo_vaga;
@@ -64,21 +64,21 @@ class VagaController extends Controller
         $vaga->modalidade_da_vaga = $request->modalidade_da_vaga;
         $vaga->id_empresas = $request->id_empresas;
         $vaga->save();
-    
+
         if ($request->has('habilidades')) {
             $vaga->habilidades()->sync($request->habilidades);
         }
-    
+
         if ($request->has('cursos')) {
             $vaga->curso()->sync($request->cursos);
         }
-    
+
         return response()->json([
             'mensagem' => 'Vaga, habilidades e cursos cadastrados com sucesso!',
             'data' => $vaga
         ], 201);
     }
-        
+
     public function adicionarHabilidades($id_habilidades, $id_vagas)
     {
 
@@ -96,7 +96,8 @@ class VagaController extends Controller
 
     }
 
-    public function adicionarCursos($id_vaga, $id_curso){
+    public function adicionarCursos($id_vaga, $id_curso)
+    {
         $curso = Curso::find($id_curso);
 
         $curso->cursoOnVaga()->attach($id_vaga);
@@ -115,18 +116,18 @@ class VagaController extends Controller
      */
     public function show(string $id)
     {
-        $vaga = Vaga::with(['habilidades', 'curso', 'empresa.pessoa.redeSocial'])->find($id);
+        $vaga = Vaga::with(['habilidades', 'curso', 'empresa.pessoa.redeSocial', 'empresa.pessoa.endereco.cidade'])->find($id);
 
         if (!$vaga) {
             return response()->json([
-                'message' => 'Vaga não encontrada' 
+                'message' => 'Vaga não encontrada'
             ], 404);
         }
 
-        
+
         return response()->json($vaga, 200);
     }
-    
+
     public function showAll(Request $request)
     {
         $modalidade = $request->input('modalidade') ? explode(",", $request->input('modalidade')) : [];
@@ -182,14 +183,14 @@ class VagaController extends Controller
         $validator = Validator::make($request->all(), $rules);
 
         if ($validator->fails()) {
-            
+
             return response()->json([
                 'error' => $validator->errors()
             ], 422);
-            
+
         }
 
-        if ($request->data_criacao > $request->data_fechamento){
+        if ($request->data_criacao > $request->data_fechamento) {
 
             return response()->json([
                 'mensagem' => 'Data de fechamento não pode ser antes da data de criação.'
@@ -217,7 +218,23 @@ class VagaController extends Controller
         ], 200);
     }
 
-    
+    public function updateStatus(string $id)
+    {
+        $vaga = Vaga::find($id);
+
+        if (!$vaga) {
+            return response()->json([
+                'mensage' => 'Vaga não encontrada'
+            ], 200);
+        }
+
+        $vaga->status = 'FINALIZADO';
+        $vaga->save();
+
+        return response()->json([
+            'mensage' => 'Vaga finalizada com sucesso'
+        ], 200);
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -232,9 +249,9 @@ class VagaController extends Controller
             ], 404);
         }
 
-        $vaga->vagaOnHabilidade()->detach();
-        $vaga->cursoOnVaga()->detach();
-        
+        $vaga->habilidades()->detach();
+        $vaga->curso()->detach();
+
         $vaga->candidato()->detach();
 
         $vaga->delete();
@@ -244,7 +261,9 @@ class VagaController extends Controller
         ], 200);
     }
 
-   
+
+
+
 
     public function verHabilidades($id_vagas)
     {
@@ -266,8 +285,8 @@ class VagaController extends Controller
     {
 
         $pessoasFisica = PessoasFisica::find($id_pessoas);
-     
-        $pessoasFisica->candidato()->attach($id_vagas, array('created_at' => Carbon::now(),'updated_at'=> Carbon::now()));
+
+        $pessoasFisica->candidato()->attach($id_vagas, array('created_at' => Carbon::now(), 'updated_at' => Carbon::now()));
 
 
         $vaga = Vaga::find($id_vagas);
@@ -298,8 +317,10 @@ class VagaController extends Controller
         }
 
 
-    return response()->json(
-         $pessoas
-    , 200);
-}
+        return response()->json(
+            $pessoas
+            ,
+            200
+        );
+    }
 }
